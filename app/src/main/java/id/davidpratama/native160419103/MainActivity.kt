177333a7dfaad60ab4e04ce6c73080b7b64821a0
daviddprtma <davidchristianpratama@gmail.com>
2022-02-09 20:27:46 +0700

    init

---
+++ app/src/main/java/id/davidpratama/native160419103/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.davidpratama.native160419103
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        test saja
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
