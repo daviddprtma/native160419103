c5492d537abe509835d5d695987d3dfbc41e3055
daviddprtma <davidchristianpratama@gmail.com>
2022-02-10 10:28:27 +0700

    toString override

--- app/src/main/java/id/davidpratama/native160419103/Login.kt
+++ app/src/main/java/id/davidpratama/native160419103/Login.kt
@@ -1,5 +1,7 @@
 package id.davidpratama.native160419103
 
-class Login(username: String, password:String) {
-
+class Login(var username: String, var password:String) {
+    override fun toString(): String {
+        return "$username $password";
+    }
 }
