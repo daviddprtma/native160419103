cafe795329a471f348a329e61a6b709200c1ae71
daviddprtma <davidchristianpratama@gmail.com>
2022-02-10 10:23:24 +0700

    ini login

--- app/src/main/java/id/davidpratama/native160419103/MainActivity.kt
+++ app/src/main/java/id/davidpratama/native160419103/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 //        test saja
+        var login: Boolean = false;
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
